fde7f06b7416630931b55c0d4c8a83ffa0c38545
root <bjoern.erik.strand@gmail.com>
2017-09-14 12:38:35 +0000

    Restructure of files and folders

--- terraform/cattle/volumes.tf
+++
@@ -1,6 +0,0 @@
-resource "rancher_volume" "mysql" {
-  name = "mysql"
-  driver = "rancher-nfs"
-  environment_id = "${rancher_environment.environment.id}"
-  depends_on = [ "rancher_stack.nfs" ]
-}
